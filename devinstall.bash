fa008406143dd5368d22b23e9e8c37f167f598b2
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-10 23:13:46 -0700

    Create devinstall.bash

---
+++ devinstall.bash
@@ -0,0 +1,8 @@
+git clone -b gh-pages https://github.com/HttpAnimation/Luker.git
+mv Luker gh-pages
+git clone -b stable https://github.com/HttpAnimation/Luker.git
+mv Luker stable
+git clone -b blank https://github.com/HttpAnimation/Luker.git
+mv Luker blank
+git clone -b main https://github.com/HttpAnimation/Luker.git
+mv Luker main
